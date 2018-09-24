1fa925d1eae71a447bfefee9e46506518ada3e06
Иван Григоренко <gradevonn@gmail.com>
2018-09-25 00:01:55 +0300

    final file lab 1

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,24 +1,45 @@
-"""
-Labour work #1
+def calculate_frequencies(info):
+    words = info.split(' ')
+    frequencies_dictionary = {}
+    for count, k in enumerate(words):
+        if words[count] in frequencies_dictionary:
+            frequencies_dictionary[words[count]] += 1
+        else:
+            frequencies_dictionary[words[count]] = 1
+    return frequencies_dictionary
 
-Count frequencies dictionary by the given arbitrary text
-"""
+
+pass
 
 
-def calculate_frequences() -> dict:
-    """
-    Calculates number of times each word appears in the text
-    """
-    pass
+def filter_stop_words(frequencies_dict, stopwords):
+    frequencies = {key: frequencies_dict[key] for key in frequencies_dict if key not in stopwords}
+    sorted_words = sorted(frequencies, key=frequencies.get)
+    sorted_words.reverse()
+    return sorted_words
 
-def filter_stop_words() -> dict:
-    """
-    Removes all stop words from the given frequencies dictionary
-    """
-    pass
 
-def get_top_n() -> tuple:
-    """
-    Takes first N popular words
-    """
-    pass
+pass
+
+
+def get_top_n(sorted_words, top_n):
+    out = ''
+    for i in range(top_n):
+        out += sorted_words[i]
+        out += '\n'
+    return out
+
+
+pass
+m_file = open("data.txt")
+final_file = open("report.txt", 'w')
+n = input(int)
+text = m_file.read(n)
+freq_dict = calculate_frequencies(text)
+stop_words = input(tuple)
+sort = filter_stop_words(freq_dict, stop_words)
+N = input(int)
+output = get_top_n(sort, N)
+final_file.write(output)
+m_file.close()
+final_file.close()
